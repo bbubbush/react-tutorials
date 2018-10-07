c7b411915f845e9f0a8aa7d1237d9b4fe03fcefd
bbubbush <bbubbush@gmail.com>
2018-10-07 19:46:08 +0900

    2018-10-07

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "Life of pie",
-    poster: "https://upload.wikimedia.org/wikipedia/en/5/57/Life_of_Pi_2012_Poster.jpg"
-  },
-  {
-    title: "About time",
-    poster: "https://upload.wikimedia.org/wikipedia/en/7/7c/About_Time_%282013_film%29_Poster.jpg"
-  },
-  {
-    title: "Avengers",
-    poster: "https://cdn3.whatculture.com/images/2018/05/c2f68e7aacf19bc2-600x400.png"
-  }
-];
-
-
 class App extends Component {
-  
+  state = {
+    movies: [
+      {
+        title: "Life of pie",
+        poster: "https://upload.wikimedia.org/wikipedia/en/5/57/Life_of_Pi_2012_Poster.jpg"
+      },
+      {
+        title: "About time",
+        poster: "https://upload.wikimedia.org/wikipedia/en/7/7c/About_Time_%282013_film%29_Poster.jpg"
+      },
+      {
+        title: "Avengers",
+        poster: "https://cdn3.whatculture.com/images/2018/05/c2f68e7aacf19bc2-600x400.png"
+      }
+    ]
+  }
+
+  componentDidMount(){
+    setTimeout(() => {
+      console.log('Hello');
+    }, 1000);
+  }
+
   render() {
     return (
       <div className="App">
-        {movies.map((movie, index) => {
+        {this.state.greeting}
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div> 
