4c13d5f18fc2f968d8068c0f033cdc1dc58e294a
bbubbush <bbubbush@gmail.com>
2018-10-07 21:09:31 +0900

    2018-10-07

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,36 +2,45 @@ import React, { Component } from 'react';
 import Movie from './Movie';
 
 class App extends Component {
-  state = {
-    movies: [
-      {
-        title: "Life of pie",
-        poster: "https://upload.wikimedia.org/wikipedia/en/5/57/Life_of_Pi_2012_Poster.jpg"
-      },
-      {
-        title: "About time",
-        poster: "https://upload.wikimedia.org/wikipedia/en/7/7c/About_Time_%282013_film%29_Poster.jpg"
-      },
-      {
-        title: "Avengers",
-        poster: "https://cdn3.whatculture.com/images/2018/05/c2f68e7aacf19bc2-600x400.png"
-      }
-    ]
-  }
+  state = {}
 
   componentDidMount(){
     setTimeout(() => {
-      console.log('Hello');
-    }, 1000);
+      console.log('setTimeout');
+      this.setState({
+        movies: [
+          {
+            title: "Life of pie",
+            poster: "https://upload.wikimedia.org/wikipedia/en/5/57/Life_of_Pi_2012_Poster.jpg"
+          },
+          {
+            title: "About time",
+            poster: "https://upload.wikimedia.org/wikipedia/en/7/7c/About_Time_%282013_film%29_Poster.jpg"
+          },
+          {
+            title: "Avengers",
+            poster: "https://cdn3.whatculture.com/images/2018/05/c2f68e7aacf19bc2-600x400.png"
+          },
+          {
+            title: 'matrix3',
+            poster: 'https://cdn3.whatculture.com/images/2018/05/c2f68e7aacf19bc2-600x400.png'
+          }
+        ]
+      });
+    }, 2000);
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading...'}
       </div> 
     );
   }
