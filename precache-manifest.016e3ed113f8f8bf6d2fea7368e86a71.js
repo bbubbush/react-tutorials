cd7b16817875d75c571b39edb9338eca0a63c7a7
bbubbush <bbubbush@gmail.com>
2018-10-07 23:57:48 +0900

    Updates

---
+++ precache-manifest.016e3ed113f8f8bf6d2fea7368e86a71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "c538455cacafba5bb13f",
+    "url": "/movie_app/static/js/main.c538455c.chunk.js"
+  },
+  {
+    "revision": "9c43fd2df6da2e32ad6f",
+    "url": "/movie_app/static/js/1.9c43fd2d.chunk.js"
+  },
+  {
+    "revision": "c538455cacafba5bb13f",
+    "url": "/movie_app/static/css/main.c461aab0.chunk.css"
+  },
+  {
+    "revision": "56fa563909a85857f955c4776e10e6ee",
+    "url": "/movie_app/index.html"
+  }
+];
