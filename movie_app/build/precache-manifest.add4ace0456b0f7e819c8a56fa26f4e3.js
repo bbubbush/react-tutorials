3a2d2187f51284af4e856a09be22f1831d6b4ca4
bbubbush <bbubbush@gmail.com>
2018-10-09 10:37:23 +0900

    2018-10-09

---
+++ movie_app/build/precache-manifest.add4ace0456b0f7e819c8a56fa26f4e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "58a917f1a3942660fa0f",
+    "url": "/movie_app/static/js/main.58a917f1.chunk.js"
+  },
+  {
+    "revision": "9c43fd2df6da2e32ad6f",
+    "url": "/movie_app/static/js/1.9c43fd2d.chunk.js"
+  },
+  {
+    "revision": "58a917f1a3942660fa0f",
+    "url": "/movie_app/static/css/main.c461aab0.chunk.css"
+  },
+  {
+    "revision": "1111d91af2a1033ac1db8d99de4a8be0",
+    "url": "/movie_app/index.html"
+  }
+];
